caca75f591658f0532a5240c47702e836908354d
Ajeet112211 <124707253+Ajeet112211@users.noreply.github.com>
2023-02-07 19:25:33 +0300

    Add files via upload

---
+++ python_fundamental_batch74_day1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("welcome to the python programming world")
+
+
+# In[5]:
+
+
+print("welcome to the python programming world")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
