b532d1eff8ba3ad61d33061da8eade9cc59d8801
Ajeet112211 <124707253+Ajeet112211@users.noreply.github.com>
2023-02-15 19:26:55 +0300

    Add files via upload

---
+++ python_fundamental_batch74_day5.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+Bike=['harley' ,'agusta' ,'honda', 'haya' ,'bajaj']   # usring the sorted method in alphabatical order temporary 
+print(sorted(Bike)) 
+
+
+# In[9]:
+
+
+Bike.sort() #usring the sort method in alphabatical order permanent
+
+
+# In[10]:
+
+
+print(Bike)
+
+
+# In[12]:
+
+
+len(Bike)   #calculatiing the lenght of the list
+
+
+# In[34]:
+
+
+student=['ajeet','divya','shubham','anju','vipul','kayu','aman']
+type(student)  #creating the list bracket matter alot in programming 
+
+
+# In[36]:
+
+
+#req:want ajeet and divya in output slicing method
+print(student[0:2])
+
+
+# In[32]:
+
+
+print(student[0:7:6])  #req: ajeet and aman print in output sliciing method.
+
+
+# In[ ]:
+
+
+
+
