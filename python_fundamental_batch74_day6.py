63db8c9be765b706d26b9253f063a218e0bba0c6
Ajeet112211 <124707253+Ajeet112211@users.noreply.github.com>
2023-02-16 18:31:53 +0300

    Add files via upload

---
+++ python_fundamental_batch74_day6.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+student=['ajeet','divya','anju','vipul','kayu','aru','arav']
+type(student)
+
+
+# In[8]:
+
+
+print(f"good work,{student[0].title()}")
+
+
+# In[44]:
+
+
+student=['ajeet','divya','anju','vipul','kayu','aru','arav']
+for x in student:
+# print(f"good work,{x}")
+ print(x)
+
+
+# In[36]:
+
+
+student=['ajeet','divya','anju','vipul','kayu','aru','arav']
+print(f"good work,{student[0].title()}")
+
+
+# In[42]:
+
+
+for x in student:
+ print(f"good work,{x}".title()) 
+ print(f"good job,{x}\n".title())
+
+
+# In[ ]:
+
+
+
+
